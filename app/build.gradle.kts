6b3d834305fcc245d7fdb4552862c64be3b3ae81
Lenovo <olegva11@gmail.com>
2024-02-25 19:44:46 +0200

    Add `KSP` for room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -56,6 +57,11 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    //room
+    implementation ("androidx.room:room-ktx:2.6.1")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    ksp("android.arch.persistence.room:compiler:1.1.1")
     //ui
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
